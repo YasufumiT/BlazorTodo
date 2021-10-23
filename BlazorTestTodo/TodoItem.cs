fa7a19f3859087508b032d1ac5f3e3fc0f264e74
YasufumiT <tnkysfm.278323@icloud.com>
2021-10-23 15:42:33 +0900

    todo first upload

---
+++ BlazorTestTodo/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlazorTestTodo
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public TodoItem()
+        {
+            this.Title = Title;
+            this.IsDone = IsDone;
+        }
+
+    }
+}
